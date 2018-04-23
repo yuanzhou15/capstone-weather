e781a79423c4ac24ba321a44aef3f6866a2374ab
Yuan Zhou <yuanzhou719@gmail.com>
2018-04-22 21:44:10 -0400

    update to configDefault file

--- yuan/src/configDefault.py
+++ yuan/src/configDefault.py
@@ -6,10 +6,15 @@ from configLocal import config as configLocal
 
 config = {
     'radarRootPath': '',
-    'satelliteRootPath': ''
+    'satelliteRootPath': '',
+    'gmapsKey': ''  # googlemaps api key, used for any maps-related code using gmaps library
 }
 
 # override common keys from local config
-for k, v in configLocal:
+for k, v in configLocal.items():
     if k in config:
         config[k] = v
+
+# files in satelliteRootPath are enumerated a lot, so we'll cache them here
+import os
+cached_listdir_sat = os.listdir(config['satelliteRootPath'])

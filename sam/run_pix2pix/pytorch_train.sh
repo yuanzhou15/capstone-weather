4b1cb4bd97fc69a0bfdf38acdd3b9e02d107f45f
sam46 <abdlwahdsa@gmail.com>
2018-09-25 08:45:32 -0400

    modify training script to make use of mutichannel loader

--- sam/run_pix2pix/pytorch_train.sh
+++ sam/run_pix2pix/pytorch_train.sh
@@ -1,4 +1,5 @@
 # arg1: root folder path
 # arg2: batch size
-python /home/wproj/pytorch-CycleGAN-and-pix2pix/train.py --dataroot $1 --checkpoints_dir $1/checkpoints --name weather --model pix2pix --direction AtoB --batch_size $2 --gpu_ids 0,1 --display_server http://crest-cluster-node-6
+# arg3: number of input channels
+python ~/pytorch-CycleGAN-and-pix2pix/train.py --dataset_mode multichannel --input_nc $3 --output_nc 1 --dataroot $1 --checkpoints_dir $1/checkpoints --name weather --model pix2pix --direction AtoB --batch_size $2 --gpu_ids 0,1 --display_server http://crest-cluster-node-6
 

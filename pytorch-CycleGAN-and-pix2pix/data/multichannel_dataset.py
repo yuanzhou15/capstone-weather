5c3dfc665f0341bec7f1658b1d9b1855b9094474
sam46 <abdlwahdsa@gmail.com>
2018-09-27 00:28:12 -0400

    fix stacked dimensions bug

--- pytorch-CycleGAN-and-pix2pix/data/multichannel_dataset.py
+++ pytorch-CycleGAN-and-pix2pix/data/multichannel_dataset.py
@@ -4,7 +4,9 @@ import torchvision.transforms as transforms
 import torch
 from data.base_dataset import BaseDataset
 from data.image_folder import make_dataset
+from random import random
 from PIL import Image
+# import numpy as np
 
 
 class MultiChannelDataset(BaseDataset):
@@ -18,15 +20,16 @@ class MultiChannelDataset(BaseDataset):
         self.dir_AB = os.path.join(opt.dataroot, opt.phase)
         self.A_paths = sorted(make_dataset(os.path.join(self.dir_AB, 'A')))
         self.B_paths = sorted(make_dataset(os.path.join(self.dir_AB, 'B')))
-        assert(opt.resize_or_crop == 'none')
+        # assert(opt.resize_or_crop == 'none')
         assert(self.opt.direction == 'AtoB')
         assert(self.opt.output_nc == 1)
-        if len(B_paths)*self.opt.input_nc != len(A_paths):
+        print('A, B counts: '+str(len(self.A_paths)) + '  '+  str(len(self.B_paths)))
+        if len(self.B_paths)*self.opt.input_nc != len(self.A_paths):
             print("warning: number of A, B images don't match. Double-check image filenames!")
 
 
     def __getitem__(self, index):
-        A_path = self.A_paths[index*self.opt.input_nc : index*(self.opt.input_nc+1)]
+        A_path = self.A_paths[index*self.opt.input_nc : (index+1)*self.opt.input_nc]
         B_path = self.B_paths[index]
         
         # if self.opt.direction == 'BtoA':
@@ -40,27 +43,30 @@ class MultiChannelDataset(BaseDataset):
         As = []
         for p in A_path:
             # each image is 8-bit channel
-            Araw = Image.open(p).convert('RGB')
+            Araw = Image.open(p).convert('RGB')#.resize((256, 256), Image.BICUBIC)
             w, h = Araw.size
             assert(w == h)
-            assert(w//4 == 0)
+            assert(w%4 == 0)
             Ai = transforms.ToTensor()(Araw)
             Ai = transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))(Ai)
             tmp = Ai[0, ...] * 0.299 + Ai[1, ...] * 0.587 + Ai[2, ...] * 0.114
             Ai = tmp.unsqueeze(0)
+            # if str(Ai.size()) != 'torch.Size([1, 256, 256])':
+            #     print('Wrong Ai shape: ' + str(Ai.size()))
             As.append(Ai)
-        A = torch.stack(As, dim=2)
+        A = torch.stack(As, dim=1)[0]
 
         # load B
-        assert (out)
-        Braw = Image.open(B_path).convert('RGB')
+        Braw = Image.open(B_path).convert('RGB')#.resize((256, 256), Image.BICUBIC)
         w, h = Braw.size
         assert(w == h)
-        assert(w//4 == 0)
+        assert(w%4 == 0)
         B = transforms.ToTensor()(Braw)
+        
         B = transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))(B)
         tmp = B[0, ...] * 0.299 + B[1, ...] * 0.587 + B[2, ...] * 0.114
         B = tmp.unsqueeze(0)
+        
 
         # if (not self.opt.no_flip) and random.random() < 0.5:
         #     # todo: what's this?
@@ -68,9 +74,12 @@ class MultiChannelDataset(BaseDataset):
         #     idx = torch.LongTensor(idx)
         #     A = A.index_select(2, idx)
         #     B = B.index_select(2, idx)
-
+        # if random() < 0.5:
+        
+        # print('A final shape: ' + str(A.size()))
+        # print('B final shape: ' + str(B.size()))
         return {'A': A, 'B': B,
-                'A_paths': A_paths[0], 'B_paths': B_path}
+                'A_paths': A_path[0], 'B_paths': B_path}
 
     def __len__(self):
         return len(self.B_paths)

0f36ae48cb00fee4daa2941baf07bdc3d815f062
sam46 <abdlwahdsa@gmail.com>
2018-09-27 00:36:41 -0400

    modify training script

--- sam/run_pix2pix/pytorch_train.sh
+++ sam/run_pix2pix/pytorch_train.sh
@@ -1,5 +1,6 @@
 # arg1: root folder path
 # arg2: batch size
 # arg3: number of input channels
-python ~/pytorch-CycleGAN-and-pix2pix/train.py --dataset_mode multichannel --input_nc $3 --output_nc 1 --dataroot $1 --checkpoints_dir $1/checkpoints --name weather --model pix2pix --direction AtoB --batch_size $2 --gpu_ids 0,1 --display_server http://crest-cluster-node-6
+# todo: add loadSize, fineSize arg?
+python ~/pytorch-CycleGAN-and-pix2pix/train.py --dataset_mode multichannel --input_nc $3 --output_nc 1 --dataroot $1 --checkpoints_dir $1/checkpoints --name weather --model pix2pix --direction AtoB --batch_size $2 --gpu_ids 0,1 --display_server http://crest-cluster-node-6 --loadSize 256 --fineSize 256
 

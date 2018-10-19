520a877da379d3964664cb0f87ca52901603af63
Airton Prado <airton.k.prado@gmail.com>
2018-10-19 09:42:42 -0400

    adding te different graphs of FBdemo

--- Airton/Visdom/FBDemo.py
+++ Airton/Visdom/FBDemo.py
@@ -272,3 +272,229 @@ try:
              opts=dict(title='{} points using WebGL'.format(webgl_num_points), webgl=True),
              win="WebGL demo")
 
+    # line updates
+    win = viz.line(
+        X=np.column_stack((np.arange(0, 10), np.arange(0, 10))),
+        Y=np.column_stack((np.linspace(5, 10, 10),
+                           np.linspace(5, 10, 10) + 5)),
+    )
+    viz.line(
+        X=np.column_stack((np.arange(10, 20), np.arange(10, 20))),
+        Y=np.column_stack((np.linspace(5, 10, 10),
+                           np.linspace(5, 10, 10) + 5)),
+        win=win,
+        update='append'
+    )
+    viz.line(
+        X=np.arange(21, 30),
+        Y=np.arange(1, 10),
+        win=win,
+        name='2',
+        update='append'
+    )
+    viz.line(
+        X=np.arange(1, 10),
+        Y=np.arange(11, 20),
+        win=win,
+        name='delete this',
+        update='append'
+    )
+    viz.line(
+        X=np.arange(1, 10),
+        Y=np.arange(11, 20),
+        win=win,
+        name='4',
+        update='insert'
+    )
+    viz.line(X=None, Y=None, win=win, name='delete this', update='remove')
+
+    viz.line(
+        X=webgl_x+1.,
+        Y=(webgl_x+1.)**3,
+        win="WebGL demo",
+        update='append',
+        opts=dict(title='{} points using WebGL'.format(webgl_num_points*2), webgl=True)
+    )
+
+    Y = np.linspace(0, 4, 200)
+    win = viz.line(
+        Y=np.column_stack((np.sqrt(Y), np.sqrt(Y) + 2)),
+        X=np.column_stack((Y, Y)),
+        opts=dict(
+            fillarea=True,
+            showlegend=False,
+            width=800,
+            height=800,
+            xlabel='Time',
+            ylabel='Volume',
+            ytype='log',
+            title='Stacked area plot',
+            marginleft=30,
+            marginright=30,
+            marginbottom=80,
+            margintop=30,
+        ),
+    )
+
+    # Assure that the stacked area plot isn't giant
+    viz.update_window_opts(
+        win=win,
+        opts=dict(
+            width=300,
+            height=300,
+        ),
+    )
+
+    # boxplot
+    X = np.random.rand(100, 2)
+    X[:, 1] += 2
+    viz.boxplot(
+        X=X,
+        opts=dict(legend=['Men', 'Women'])
+    )
+
+    # stemplot
+    Y = np.linspace(0, 2 * math.pi, 70)
+    X = np.column_stack((np.sin(Y), np.cos(Y)))
+    viz.stem(
+        X=X,
+        Y=Y,
+        opts=dict(legend=['Sine', 'Cosine'])
+    )
+
+    # quiver plot
+    X = np.arange(0, 2.1, .2)
+    Y = np.arange(0, 2.1, .2)
+    X = np.broadcast_to(np.expand_dims(X, axis=1), (len(X), len(X)))
+    Y = np.broadcast_to(np.expand_dims(Y, axis=0), (len(Y), len(Y)))
+    U = np.multiply(np.cos(X), Y)
+    V = np.multiply(np.sin(X), Y)
+    viz.quiver(
+        X=U,
+        Y=V,
+        opts=dict(normalize=0.9),
+    )
+
+    # pie chart
+    X = np.asarray([19, 26, 55])
+    viz.pie(
+        X=X,
+        opts=dict(legend=['Residential', 'Non-Residential', 'Utility'])
+    )
+
+    # scatter plot example with various type of updates
+    colors = np.random.randint(0, 255, (2, 3,))
+    win = viz.scatter(
+        X=np.random.rand(255, 2),
+        Y=(np.random.rand(255) + 1.5).astype(int),
+        opts=dict(
+            markersize=10,
+            markercolor=colors,
+            legend=['1', '2']
+        ),
+    )
+
+    viz.scatter(
+        X=np.random.rand(255),
+        Y=np.random.rand(255),
+        opts=dict(
+            markersize=10,
+            markercolor=colors[0].reshape(-1, 3),
+
+        ),
+        name='1',
+        update='append',
+        win=win)
+
+    viz.scatter(
+        X=np.random.rand(255, 2),
+        Y=(np.random.rand(255) + 1.5).astype(int),
+        opts=dict(
+            markersize=10,
+            markercolor=colors,
+        ),
+        update='append',
+        win=win)
+
+    # mesh plot
+    x = [0, 0, 1, 1, 0, 0, 1, 1]
+    y = [0, 1, 1, 0, 0, 1, 1, 0]
+    z = [0, 0, 0, 0, 1, 1, 1, 1]
+    X = np.c_[x, y, z]
+    i = [7, 0, 0, 0, 4, 4, 6, 6, 4, 0, 3, 2]
+    j = [3, 4, 1, 2, 5, 6, 5, 2, 0, 1, 6, 3]
+    k = [0, 7, 2, 3, 6, 7, 1, 1, 5, 5, 7, 6]
+    Y = np.c_[i, j, k]
+    viz.mesh(X=X, Y=Y, opts=dict(opacity=0.5))
+
+    # SVG plotting
+    svgstr = """
+    <svg height="300" width="300">
+      <ellipse cx="80" cy="80" rx="50" ry="30"
+       style="fill:red;stroke:purple;stroke-width:2" />
+      Sorry, your browser does not support inline SVG.
+    </svg>
+    """
+    viz.svg(
+        svgstr=svgstr,
+        opts=dict(title='Example of SVG Rendering')
+    )
+
+    # close text window:
+    viz.close(win=textwindow)
+
+    # assert that the closed window doesn't exist
+    assert not viz.win_exists(textwindow), 'Closed window still exists'
+
+    # Arbitrary visdom content
+    trace = dict(x=[1, 2, 3], y=[4, 5, 6], mode="markers+lines", type='custom',
+                 marker={'color': 'red', 'symbol': 104, 'size': "10"},
+                 text=["one", "two", "three"], name='1st Trace')
+    layout = dict(title="First Plot", xaxis={'title': 'x1'},
+                  yaxis={'title': 'x2'})
+
+    viz._send({'data': [trace], 'layout': layout, 'win': 'mywin'})
+
+    # PyTorch tensor
+    try:
+        import torch
+        viz.line(Y=torch.Tensor([[0., 0.], [1., 1.]]))
+    except ImportError:
+        print('Skipped PyTorch example')
+
+    # audio demo:
+    tensor = np.random.uniform(-1, 1, 441000)
+    viz.audio(tensor=tensor, opts={'sample_frequency': 441000})
+
+    # audio demo:
+    # download from http://www.externalharddrive.com/waves/animal/dolphin.wav
+    try:
+        audio_url = 'http://www.externalharddrive.com/waves/animal/dolphin.wav'
+        # linux
+        if _platform == "linux" or _platform == "linux2":
+            audiofile = '/home/%s/dolphin.wav' % getpass.getuser()
+        # MAC OS X
+        elif _platform == "darwin":
+            audiofile = '/Users/%s/dolphin.wav' % getpass.getuser()
+        # download audio
+        urllib.request.urlretrieve(audio_url, audiofile)
+
+        if os.path.isfile(audiofile):
+            viz.audio(audiofile=audiofile)
+    except BaseException:
+        print('Skipped audio example')
+
+    try:
+        input = raw_input  # for Python 2 compatibility
+    except NameError:
+        pass
+    input('Waiting for callbacks, press enter to quit.')
+except BaseException as e:
+    print(
+        "The visdom experienced an exception while running: {}\n"
+        "The demo displays up-to-date functionality with the GitHub version, "
+        "which may not yet be pushed to pip. Please upgrade using "
+        "`pip install -e .` or `easy_install .`\n"
+        "If this does not resolve the problem, please open an issue on "
+        "our GitHub.".format(repr(e))
+    )
